14e0b625bcd1fab7e717b9d1f012477c80fb49fa
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-04-10 10:37:34 +0200

    creating a structure

---
+++ src/js/slider.js
@@ -0,0 +1,8 @@
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+export const mySwiper = new Swiper(swiperContainer, parameters);
+
+// swiperContainer - HTMLElement or string (with CSS Selector) of swiper container HTML element. Required.
+// parameters - object - object with Swiper parameters. Optional.
+// Method returns initialized Swiper instance
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/about-me';
+import './js/projects';
+import './js/faq';
+import './js/covers';
+import './js/reviews';
+import './js/work-together';
+import './js/mobile-menu';
