e0313474392b277cf2cd85863cf11de99d062fe3
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-04-14 10:04:05 +0200

    fix js header

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,9 @@
 
-    const menuBtn = document.querySelector(".menu-btn");
+const menuBtn = document.querySelector(".menu-btn");
 const menuNav = document.querySelector(".menu-nav");
 const menuItems = document.querySelectorAll(".menu-nav li a");
-    const orderButton = document.querySelector(".order-button");
+const orderButton = document.querySelector(".order-button");
+
   
     function toggleMenu() {
       menuNav.classList.toggle("show-menu");
@@ -12,8 +13,23 @@ const menuItems = document.querySelectorAll(".menu-nav li a");
       toggleMenu(); 
     });
       
-    menuItems.forEach(function(item) {
-      item.addEventListener("click", function() {
-        toggleMenu(); 
-      });
-    });
+    menuItems.forEach(link => link.addEventListener("click", scrollToSection));
+ 
+function scrollToSection(event) {
+  toggleMenu();
+  
+  // const targetId = this.getAttribute('href').substring(1);
+  // const sectionRef = document.querySelector(`.${targetId}`);
+
+  // console.log(targetId);
+  // console.log(sectionRef);
+  // console.log(sectionRef.offsetTop);
+
+  // if (sectionRef) (
+  //   window.scrollBy({
+  //   top: sectionRef.offsetTop,
+  //   behavior: 'smooth',
+  // })
+  // )
+}
+
