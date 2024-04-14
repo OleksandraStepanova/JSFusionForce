5ce97a8c72758d07f6cb643b5ebcbf8cda94f1e4
ivankaaaaaaa <148864725+ivankaaaaaaa@users.noreply.github.com>
2024-04-14 15:27:54 +0200

    FAQ-js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,25 +1,31 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 document.addEventListener('DOMContentLoaded', function () {
-    const accordionButtons = document.querySelectorAll('.accordion-button');
-  
-    accordionButtons.forEach(button => {
-      button.addEventListener('click', () => {
-        const accordionItem = button.closest('.faq-element');
-        const accordionContent = accordionItem.querySelector('.accordion-content');
-  
-        // Змінюємо класи для зміни стану акордеону
-        accordionContent.classList.toggle('active');
-  
-        // Змінюємо іконку кнопки згідно зі станом акордеону
-        const icon = button.querySelector('.accordion-button-icon');
-        icon.classList.toggle('active');
-  
-        // Показуємо або приховуємо вміст питання
-        if (accordionContent.classList.contains('active')) {
-          accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-        } else {
-          accordionContent.style.maxHeight = null;
-        }
-      });
+  const accordionButtons = document.querySelectorAll('.accordion-button');
+
+  accordionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const accordionItem = button.closest('.faq-element');
+      const accordionContent =
+        accordionItem.querySelector('.accordion-content');
+
+      accordionContent.classList.toggle('active');
+
+      const icon = button.querySelector('.accordion-button-icon');
+      icon.classList.toggle('active');
+
+      const useElement = button.querySelector('.accordion-button-icon use');
+      if (accordionContent.classList.contains('active')) {
+        useElement.setAttribute('href', './img/icons.svg#icon-less');
+      } else {
+        useElement.setAttribute('href', './img/icons.svg#icon-more');
+      }
+
+      if (accordionContent.classList.contains('active')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null;
+      }
     });
   });
-
+});
