8a66768fdb163393eec38b49ddab957f07249576
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-04-14 08:43:28 +0200

    fix header

--- src/js/header.js
+++ src/js/header.js
@@ -1,49 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+
     const menuBtn = document.querySelector(".menu-btn");
-    const menuNav = document.querySelector(".menu-nav");
+const menuNav = document.querySelector(".menu-nav");
+const menuItems = document.querySelectorAll(".menu-nav li a");
+    const orderButton = document.querySelector(".order-button");
   
     function toggleMenu() {
-      menuNav.classList.toggle("visually-hidden");
+      menuNav.classList.toggle("show-menu");
     }
   
     menuBtn.addEventListener("click", function() {
       toggleMenu(); 
     });
-  
-    const menuItems = document.querySelectorAll(".menu-nav li a");
+      
     menuItems.forEach(function(item) {
       item.addEventListener("click", function() {
         toggleMenu(); 
       });
     });
-  });
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const menuLinks = document.querySelectorAll(".menu-nav li a");
-    const orderButton = document.querySelector(".order-button");
-  
-    function smoothScroll(targetId) {
-      const targetSection = document.querySelector(targetId);
-      if (targetSection) {
-        const offsetTop = targetSection.offsetTop;
-        window.scrollTo({
-          top: offsetTop,
-          behavior: "smooth"
-        });
-      }
-    }
-  
-    menuLinks.forEach(function(link) {
-      link.addEventListener("click", function(event) {
-        event.preventDefault(); 
-        const targetId = this.getAttribute("href");
-        smoothScroll(targetId);
-      });
-    });
-  
-    orderButton.addEventListener("click", function(event) {
-      event.preventDefault();
-      const targetId = this.getAttribute("href");
-      smoothScroll(targetId);
-    });
-  });
