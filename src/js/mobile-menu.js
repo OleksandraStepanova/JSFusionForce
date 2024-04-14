22d406740c2c79164aa9ba165fcf6ef20a100b4d
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-04-14 16:41:35 +0200

    update

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,27 +3,26 @@ const burgerMenu = document.querySelector('.mobile-menu');
 const closeBtn = document.querySelector('.mobile-menu-svg');
 
 function openMenu() {
-  if (burgerMenu.style.right !== '-100%') {
-    burgerMenu.style.right = '0';
-    burgerMenu.style.display = 'flex';
-  } else {
-    burgerMenu.style.right = '0';
-    burgerMenu.style.display = 'none';
-  }
-  return 0;
+if (burgerMenu.style.right !== '-100%') {
+ burgerMenu.style.right = '0';
+ burgerMenu.style.display = 'flex';
+} else {
+ burgerMenu.style.right = '0';
+ burgerMenu.style.display = 'none';
+ }
+return 0;
 }
 
 function closeMenu() {
-  burgerMenu.style.right = '-100%';
+ burgerMenu.style.right = '-100%';
 }
 
 const menuLinks = document.querySelectorAll('.mobile-menu-part');
 menuLinks.forEach(links => {
-  links.addEventListener('click', () => {
-    closeMenu();
-  });
+ links.addEventListener('click', () => {
+ closeMenu();
 });
-
+});
 
 mobileBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
