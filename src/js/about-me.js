0b9f7416234320367abd5aab694685a75266a8d7
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-04-14 13:10:00 +0200

    help with Swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -2,30 +2,24 @@
 // import 'swiper/css';
 
 import Swiper from 'swiper';
-import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
-Swiper.use([Navigation, Keyboard, Mousewheel]);
+import { Navigation, Keyboard, Mousewheel, Pagination } from 'swiper/modules';
+Swiper.use([Navigation, Keyboard, Mousewheel, Pagination]);
 
 import 'swiper/css/bundle';
 
 export const aboutMeSwiper = new Swiper('.swiper-about-me', {
-  slidesPerView: 6,
-  spaceBetween: 0,
-  //   slidesPerGroup: 6,
-
-  breakpoints: {
-    320: {
-      slidesPerView: 5,
-      spaceBetween: 0,
+    slidesPerView: 6,
+    breakpoints:{
+        375: {
+            slidesPerView: 5,
+        },
+        768: {
+            slidesPerView: 5,
+        },
+        1440: {
+            slidesPerView: 5,
+        }
     },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 0,
-    },
-    1440: {
-      slidesPerView: 5,
-      spaceBetween: 0,
-    },
-  },
 
   initialSlide: 0,
   loop: true,
@@ -37,17 +31,18 @@ export const aboutMeSwiper = new Swiper('.swiper-about-me', {
   mousewheel: {
     invert: true,
   },
-  //   navigation: {
-  //     nextEl: '.swiper-button-prev',
-  //   },
-  on: {
-    slideChange: function () {
-      //   let prevSlide = this.slides[this.previousIndex];
-      //   prevSlide.style.backgroundColor = '';
-      let currentSlide = this.slides[this.activeIndex];
-      currentSlide.style.backgroundColor = '#ED3B44';
-      let currentNextSlide = this.slides[this.activeIndex + 1];
-      currentNextSlide.style.backgroundColor = '';
-    },
+  navigation: {
+  nextEl: '.swiper-button-next',
   },
+  effect: 'fade',
+  allowSlidePrev: false,
+  on: {
+        slideChange: function () {
+            let currentSlide = this.slides[this.activeIndex];
+            currentSlide.style.backgroundColor = 'transparent';
+            let currentNextSlide = this.slides[this.activeIndex + 1];
+            currentNextSlide.style.backgroundColor = '#ED3B44';
+           
+        }
+    }
 });
