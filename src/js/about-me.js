d5c794d84320207e1f4b2bb4e644c3c24ad32d03
TetyanaLugova <lugovatanya1503@gmail.com>
2024-04-15 11:21:55 +0300

    Accordion-finish

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,26 +1,25 @@
 // import Swiper from 'swiper';
 // import 'swiper/css';
-
 import Swiper from 'swiper';
 import { Navigation, Keyboard, Mousewheel, Pagination } from 'swiper/modules';
 Swiper.use([Navigation, Keyboard, Mousewheel, Pagination]);
 
 import 'swiper/css';
+import Accordion from 'accordion-js';
 
 export const aboutMeSwiper = new Swiper('.swiper-about-me', {
-    slidesPerView: 'auto',
-    breakpoints:{
-        375: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        1440: {
-            slidesPerView: 5,
-        }
+  slidesPerView: 'auto',
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 5,
+    },
   },
-    
 
   initialSlide: 0,
   loop: true,
@@ -28,7 +27,7 @@ export const aboutMeSwiper = new Swiper('.swiper-about-me', {
     delay: 5000,
   },
   grabCursor: true,
-    keyboard: {
+  keyboard: {
     enabled: true,
     onlyInViewport: true,
   },
@@ -42,10 +41,34 @@ export const aboutMeSwiper = new Swiper('.swiper-about-me', {
   effect: 'fade',
   allowSlidePrev: false,
   on: {
-        slideChange: function () {
-      document.querySelector('.swiper-slide.skills-item-wrap-activ').classList.remove('skills-item-wrap-activ');
-      this.slides[this.activeIndex].classList.add('skills-item-wrap-activ')
-           
-        }
+    slideChange: function () {
+      document
+        .querySelector('.swiper-slide.skills-item-wrap-activ')
+        .classList.remove('skills-item-wrap-activ');
+      this.slides[this.activeIndex].classList.add('skills-item-wrap-activ');
+    },
+  },
+});
+
+const buttons = document.querySelectorAll('.ac-trigger');
+const icons = document.querySelectorAll('.about-me-icon');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const icon = icons[index];
+    const isOpen = icon.classList.contains('about-me-icon-open');
+    if (isOpen) {
+      icon.classList.remove('about-me-icon-open');
+    } else {
+      icon.classList.add('about-me-icon-open');
     }
+  });
+});
+
+new Accordion('.accordion-container', {
+  duration: 400,
+  showMultiple: false,
+  activeClass: 'is-active',
+  showMultiple: true,
+  openOnInit: [0],
 });
