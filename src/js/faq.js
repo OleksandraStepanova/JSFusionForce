df22921148e4fc1a94e02c8739339fbb9296b3a8
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-04-18 17:24:24 +0200

    add styles

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,3 +4,19 @@ new Accordion('.accordion-container-faq', {
   duration: 400,
   showMultiple: false,
 });
+
+
+const buttonsFaq = document.querySelectorAll('.ac-trigger-faq');
+const iconsFaq = document.querySelectorAll('.accordion-button-icon');
+
+buttonsFaq.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const icon = iconsFaq[index];
+    const isOpen = icon.classList.contains('about-me-icon-open');
+    if (isOpen) {
+      icon.classList.remove('about-me-icon-open');
+    } else {
+      icon.classList.add('about-me-icon-open');
+    }
+  });
+});
